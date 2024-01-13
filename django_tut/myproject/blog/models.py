582f762c1dbc8e8308ae62a97014399192975d0c
mrisis <rezaamin8889@gmail.com>
2024-01-13 16:19:57 +0330

    farsi admin panle and fields models and change time zone iran

--- django_tut/myproject/blog/models.py
+++ django_tut/myproject/blog/models.py
@@ -7,14 +7,18 @@ class Article(models.Model):
         ('d', 'draft'),
         ('p', 'published')
     )
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField()
-    thumbnail = models.ImageField(upload_to='images')
-    publish = models.DateTimeField(default=timezone.now())
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    title = models.CharField(max_length=200,  verbose_name ='عنوان')
+    slug = models.SlugField(max_length=100, unique=True,  verbose_name = 'اسلاگ')
+    description = models.TextField(verbose_name='بدنه')
+    thumbnail = models.ImageField(upload_to='images',  verbose_name ='تصویر')
+    publish = models.DateTimeField(default=timezone.now(),  verbose_name ='انتشار')
+    created = models.DateTimeField(auto_now_add=True,  verbose_name ='ساخته شده در ')
+    updated = models.DateTimeField(auto_now=True,  verbose_name ='بروزرسانی شده در')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES,  verbose_name ='وضعیت')
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
 
     def __str__(self):
         return self.title
