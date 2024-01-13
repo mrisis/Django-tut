cb46754632217fc86b15a9b99ece38f650614522
mrisis <rezaamin8889@gmail.com>
2024-01-13 16:20:57 +0330

    back to utc time zone and english admin panel

--- django_tut/myproject/config/settings.py
+++ django_tut/myproject/config/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'fa-ir'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Tehran'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
