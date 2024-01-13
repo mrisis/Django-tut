9e5a73e9662f42c95b6c4486b4b28f8dda4e9c4a
mrisis <rezaamin8889@gmail.com>
2024-01-13 16:02:28 +0330

    change query article does not exist handel error 404

--- django_tut/myproject/blog/views.py
+++ django_tut/myproject/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from .models import Article
 
@@ -29,7 +29,7 @@ def home(request):
 
 def detail(request, slug):
     context = {
-        'article': Article.objects.get(slug=slug)
+        'article': get_object_or_404(Article, slug=slug)
     }
     return render(request, 'blog/single.html', context)
 
