5763a5693ba1bc307ee993f7be2fd6e2fc121fc5
mrisis <rezaamin8889@gmail.com>
2024-01-14 11:22:46 +0330

    write function for change numbers to persian nmbers

--- django_tut/myproject/extenstions/utils.py
+++ django_tut/myproject/extenstions/utils.py
@@ -2,6 +2,24 @@ from . import jalali
 from django.utils import timezone
 
 
+def persian_numbers_converter(mystr):
+    numbers = {
+        "0": "۰",
+        "1": "۱",
+        "2": "۲",
+        "3": "۳",
+        "4": "۴",
+        "5": "۵",
+        "6": "۶",
+        "7": "۷",
+        "8": "۸",
+        "9": "۹",
+    }
+    for e, p in numbers.items():
+        mystr = mystr.replace(e, p)
+    return mystr
+
+
 def jalali_convertor(time):
     j_months = ['فروردین','اردیبشبهت','خرداد','تیر','مرداد','شهریور','مهر','آبان','آذر','دی','بهمن','اسفند',]
     time = timezone.localtime(time)
@@ -20,4 +38,6 @@ def jalali_convertor(time):
             time.hour,
             time.minute
         )
+    return persian_numbers_converter(output)
+
 
