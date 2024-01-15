4a234127b32e13daa5be471855b91de8ed26e0bd
mrisis <rezaamin8889@gmail.com>
2024-01-15 14:21:49 +0330

    write action for publish and draft articles

--- django_tut/myproject/blog/admin.py
+++ django_tut/myproject/blog/admin.py
@@ -12,12 +12,38 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 
 
+def make_published(modeladmin, request, queryset):
+    rows_updated = queryset.update(status='p')
+    if rows_updated == 1:
+        message_bit = 'منتشر شد'
+    else:
+        message_bit = 'منشتر شدند'
+    modeladmin.message_user(request, '{} مقاله {}'.format(rows_updated, message_bit))
+
+
+make_published.short_discriotion = 'انتشار مقالات انتخاب شده'
+
+
+def make_draft(modeladmin, request, queryset):
+    rows_updated = queryset.update(status='d')
+    if rows_updated == 1:
+        message_bit = 'پیش نویس شد'
+    else:
+        message_bit = 'پیش نویس شدند'
+    modeladmin.message_user(request, '{} مقاله {}'.format(rows_updated, message_bit))
+
+
+make_published.short_discriotion = 'پیش نویس مقالات انتخاب شده'
+
+
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'jpublish', 'status', 'category_to_str')
     list_filter = ('publish', 'status')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['-publish', 'status']
+    actions = [make_published, make_draft]
 
     def category_to_str(self, obj):
         return ", ".join([category.title for category in obj.category_published()])
